6c6cc5b789193ea1b91531498f778ff788122c44
Alejandro Abdelnur <tucu@cloudera.com>
2012-02-06 10:40:32 -0800

    MR1: MAPREDUCE-3727 jobtoken location property in jobconf refers to wrong jobtoken file.
      This backport is backporting also a portion of HADOOP-7001 (Configuration#unset method)
    
      Reason: Oozie Hive actions are impacted by this bug
      Author: Alejandro Abdelnur
      Ref: CDH-4232
    (cherry picked from commit 58cc8c9b3d8699bf6d866689009c140dc5cbd82a)
    
    (cherry picked from commit caadc4e3118ae07d0e3dabcf2e999cba07ad9df1)

--- src/test/org/apache/hadoop/mapreduce/security/TestTokenCache.java
+++ src/test/org/apache/hadoop/mapreduce/security/TestTokenCache.java
@@ -19,6 +19,7 @@ package org.apache.hadoop.mapreduce.security;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -287,4 +288,15 @@ public class TestTokenCache {
       assertTrue("didn't find token for " + p1 ,found);
     }
   }
+
+
+  @Test
+  public void testCleanUpTokenReferral() throws Exception {
+    Configuration conf = new Configuration();
+    conf.set(TokenCache.MAPREDUCE_JOB_CREDENTIALS_BINARY, "foo");
+    TokenCache.cleanUpTokenReferral(conf);
+    assertNull(conf.get(TokenCache.MAPREDUCE_JOB_CREDENTIALS_BINARY));
+  }
+
+
 }
