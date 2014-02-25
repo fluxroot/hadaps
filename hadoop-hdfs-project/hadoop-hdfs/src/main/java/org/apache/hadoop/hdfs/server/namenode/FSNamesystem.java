9657573862658ed25cdbaba6b10c240752c65bd7
Chris Nauroth <cnauroth@apache.org>
2014-02-25 20:27:43 +0000

    HDFS-6006. Remove duplicate code in FSNameSystem#getFileInfo. Contributed by Akira Ajisaka.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-2@1571815 13f79535-47bb-0310-9956-ffa450edef68
    (cherry picked from commit 7794e76bf539a3061df24d2b47624e638bbae66d)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -3417,9 +3417,6 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
     HdfsFileStatus stat = null;
     FSPermissionChecker pc = getPermissionChecker();
     checkOperation(OperationCategory.READ);
-    if (!DFSUtil.isValidName(src)) {
-      throw new InvalidPathException("Invalid file name: " + src);
-    }
     byte[][] pathComponents = FSDirectory.getPathComponentsForReservedPath(src);
     readLock();
     try {
